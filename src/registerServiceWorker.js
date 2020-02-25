75088a293bfda98aab69bbca504cf4def720deeb
metheany <metheany.thorn@gmail..com>
2020-02-25 10:43:23 +0700

    update

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -12,7 +12,6 @@ if (process.env.NODE_ENV === 'production') {
         },
         registered() {
             console.log('Service worker has been registered.')
-            alert('Service worker has been registered.')
         },
         cached() {
             console.log('Content has been cached for offline use.')
@@ -20,19 +19,15 @@ if (process.env.NODE_ENV === 'production') {
         },
         updatefound() {
             console.log('New content is downloading.')
-            alert('New content is downloading.')
         },
         updated() {
             console.log('New content is available; please refresh.')
-            alert('New content is available; please refresh.')
         },
         offline() {
             console.log('No internet connection found. App is running in offline mode.')
-            alert('No internet connection found. App is running in offline mode.')
         },
         error(error) {
             console.error('Error during service worker registration:', error)
-            alert('Error during service worker registration:', error)
         }
     })
 
