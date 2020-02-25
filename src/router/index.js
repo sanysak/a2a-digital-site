c0505b9334d99a66998d4744528737b101432647
metheany <metheany.thorn@gmail..com>
2020-02-25 08:17:16 +0700

    update route to perform page load speed

--- src/router/index.js
+++ src/router/index.js
@@ -1,82 +1,71 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HomePage from '../views/HomePage.vue'
-import SolutionDetail from '../views/SolutionDetail.vue'
-import AboutPage from '../views/AboutPage.vue'
-import ContactPage from '../views/ContactPage.vue'
-import JobsPage  from '../views/JobsPage.vue'
-import JobsPageDetail from '../views/JobsPageDetail.vue'
-import JobsSubmitPage from '../views/JobsSubmitPage.vue'
-import ErrorPage from '../views/ErrorPage.vue'
-import ServiceDetailPage from '../views/ServiceDetailPage.vue'
-import ProfilePage from '../views/ProfilePage.vue'
-import SuccessfulPage from '../views/SuccessfulPage.vue'
 
 Vue.use(Router)
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomePage
-    },
-    {
-      path: '/solutiondetail/:solution',
-      name: 'solutiondetail',
-      component: SolutionDetail
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: AboutPage
-    },
-    {
-      path: '/contact',
-      name: 'contact',
-      component: ContactPage
-    },
-    {
-      path: '/jobs',
-      name: 'jobs',
-      component: JobsPage
-    },
-    {
-      path: '/jobsdetail',
-      name: 'jobsdetail',
-      component: JobsPageDetail
-    },
-    {
-      path: '/jobform',
-      name: 'jobform',
-      component: JobsSubmitPage
-    },
-    {
-      path: '/servicedetail/:service',
-      name: 'servicedetail',
-      component: ServiceDetailPage,
-    },
-    {
-      path: '/profile',
-      name: 'profile',
-      component: ProfilePage
-    },
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: () => import(/* webpackChunkName: "HomePage" */ '@/views/HomePage.vue')
+        },
+        {
+            path: '/solutiondetail/:solution',
+            name: 'solutiondetail',
+            component: () => import(/* webpackChunkName: "SolutionDetail" */ '@/views/SolutionDetail.vue')
+        },
+        {
+            path: '/about',
+            name: 'about',
+            component: () => import(/* webpackChunkName: "About" */ '@/views/AboutPage.vue')
+        },
+        {
+            path: '/contact',
+            name: 'contact',
+            component: () => import(/* webpackChunkName: "Contact" */ '@/views/ContactPage.vue')
+        },
+        {
+            path: '/jobs',
+            name: 'jobs',
+            component: () => import(/* webpackChunkName: "Jobs" */ '@/views/JobsPage.vue')
+        },
+        {
+            path: '/jobsdetail',
+            name: 'jobsdetail',
+            component: () => import(/* webpackChunkName: "JobsDetail" */ '@/views/JobsPageDetail.vue')
+        },
+        {
+            path: '/jobform',
+            name: 'jobform',
+            component: () => import(/* webpackChunkName: "JobsSubmit" */ '@/views/JobsSubmitPage.vue')
+        },
+        {
+            path: '/servicedetail/:service',
+            name: 'servicedetail',
+            component: () => import(/* webpackChunkName: "ServiceDetail" */ '@/views/ServiceDetailPage.vue')
+        },
+        {
+            path: '/profile',
+            name: 'profile',
+            component: () => import(/* webpackChunkName: "Profile" */ '@/views/ProfilePage.vue')
+        },
 
-    {
-      path: '/success',
-      name: 'success',
-      component: SuccessfulPage
-    },
-    {
-      path: '*',
-      name: '404',
-      component: ErrorPage
-    },
-    
-  ],
-  scrollBehavior() {
-    window.scrollTo(0,0);
-  }
+        {
+            path: '/success',
+            name: 'success',
+            component: () => import(/* webpackChunkName: "SuccessSendMail" */ '@/views/SuccessfulPage.vue')
+        },
+        {
+            path: '*',
+            name: '404',
+            component: () => import(/* webpackChunkName: "Error" */ '@/views/ErrorPage.vue')
+        },
+
+    ],
+    scrollBehavior() {
+        window.scrollTo(0, 0);
+    }
 })
