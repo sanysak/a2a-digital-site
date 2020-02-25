d0b5cfb1ecdc55c3ad808ea17c2e021b48c5aa82
Thaung Monyodam <you@example.com>
2020-02-25 11:57:20 +0700

    pre load

--- src/main.js
+++ src/main.js
@@ -47,7 +47,8 @@ new Vue({
   created () {
     AOS.init()
   },
-  render: h => h(App)
+  render: h => h(App),
+  mounted: () => document.dispatchEvent(new Event("x-app-rendered")),
 }).$mount('#app')
 
 window.router = router
--- vue.config.js
+++ vue.config.js
@@ -3,5 +3,26 @@ module.exports = {
         workboxOptions: {
             skipWaiting: true
         }
-    }
+    },
+
+    pluginOptions: {
+        prerenderSpa: {
+          registry: undefined,
+          renderRoutes: [
+            '/',
+            '/about',
+            '/contact'
+          ],
+          useRenderEvent: true,
+          headless: true,
+          onlyProduction: true,
+          postProcess: route => {
+            // Defer scripts and tell Vue it's been server rendered to trigger hydration
+            route.html = route.html
+              .replace(/<script (.*?)>/g, '<script $1 defer>')
+              .replace('id="app"', 'id="app" data-server-rendered="true"');
+            return route;
+          }
+        }
+      }
 }
